e591104d8d515029fd467c18790c9eec7c14109a
Ben Cameron <camer343@umn.edu>
2020-08-08 22:36:23 -0500

    Problems with scedule timing. Heckers maybe use new api i downloaded

--- API_2020.py
+++ API_2020.py
@@ -2,10 +2,12 @@ import requests
 import json
 import calendar
 import time
+import datetime
 from math import floor
 import geocoder
 import random
 import csv
+
 
 def calc_days():
     '''Takes the number of seconds that we have 
@@ -32,18 +34,14 @@ def random_quote(num):
             return quote
         else:
             i+=1
-def main():
-    '''quotes= ["Change your thoughts and you change your world; Norman Vincent Peale",
-            "It's a beautiful day to save lives;Derek Shepard",
-            "I can't change the direction of the wind, but I can adjust my sails to always reach my destination;Jimmy Dean"]'''
-    
+
+def weather():
     #The url for the api request	
     weather_url = "https://community-open-weather-map.p.rapidapi.com/weather"
 
     #lat and lon coords to call with api
     lat,lon = geocoder.ip('me').latlng
-
-    UWM_coords=(43.076070,-87.882350)
+    #UWM_coords=(43.076070,-87.882350)
 
     #make and set our query init to (0,0) if coords can't be found
     querystring = {"lat":"0","lon":"0","units":"imperial"}
@@ -53,24 +51,65 @@ def main():
     headers = {
         'x-rapidapi-key': "c3a8bf4f4dmsh80a23d2f83e4ad1p13093fjsn898b5d24848b"
         }
-
     temp_data = requests.request("GET", weather_url, headers=headers, params=querystring)
     temp_data=json.loads(temp_data.content)
     #1 hPa -> 0.029529983071445 inHg
+    return temp_data
 
-    ''' Probably have to pay for this, so heck
+'''
+def main():
+    '''quotes= ["Change your thoughts and you change your world; Norman Vincent Peale",
+            "It's a beautiful day to save lives;Derek Shepard",
+            "I can't change the direction of the wind, but I can adjust my sails to always reach my destination;Jimmy Dean"]'''
+    
+    #find minute and hour for when to call funcs
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    hour=int(current_time[:2])
+    minute=int(current_time[3:5])
+    second=int(current_time[7:9])
+    print(second)
+
+    rand_int=random.randint(0,75965)
+    rand_quote=random_quote(rand_int)
+
+    #Every five run this
+    if(minute%5==0):
+        #The url for the api request	
+        weather_url = "https://community-open-weather-map.p.rapidapi.com/weather"
+
+        #lat and lon coords to call with api
+        lat,lon = geocoder.ip('me').latlng
+
+        #UWM_coords=(43.076070,-87.882350)
+
+        #make and set our query init to (0,0) if coords can't be found
+        querystring = {"lat":"0","lon":"0","units":"imperial"}
+        querystring["lat"]=str(lat)
+        querystring["lon"]=str(lon)
+
+        headers = {
+            'x-rapidapi-key': "c3a8bf4f4dmsh80a23d2f83e4ad1p13093fjsn898b5d24848b"
+            }
+        temp_data = requests.request("GET", weather_url, headers=headers, params=querystring)
+        temp_data=json.loads(temp_data.content)
+        #1 hPa -> 0.029529983071445 inHg
+
+    
+    #Probably have to pay for this, so heck
     orig_coord = lat, lon
     dest_coord = UWM_coords[0], UWM_coords[1]
     url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(orig_coord),str(dest_coord))
     drive=requests.request("GET", url)
     drive=json.loads(drive.content)
     print(drive)
-    '''
+    
     #use the csv to grab a random quote
-    rand_quote=random.randint(0,75965)
-    rand_quote=random_quote(rand_quote)
+    rand_int=random.randint(0,75965)
+    rand_quote=random_quote(rand_int)
 
     return temp_data,calc_days(),rand_quote
 
 if __name__ == "__main__":
     main()
+    '''
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import atexit
 import API_2020
 
 app = Flask(__name__)
@@ -7,4 +8,4 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     temp,days,quote=API_2020.main()
-    return render_template('index.html',days=days,quote=quote)
+    return render_template('index.html',temp=temp, days=days,quote=quote)
